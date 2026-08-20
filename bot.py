9aeda71ddcc6904a713aa8b7070c734f1f054bd5
daryzryzhova <ryzhovada1@yandex.ru>
2026-08-20 13:05:18 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,14 +3,14 @@ from vkbottle.bot import Bot, Message
 
 VK_TOKEN = os.getenv("VK_TOKEN")
 if not VK_TOKEN:
-    print("Ошибка: VK_TOKEN не задан")
+    print("Ошибка: не задан VK_TOKEN")
     exit(1)
 
 bot = Bot(VK_TOKEN)
 
 @bot.on.message()
 async def handle_message(message: Message):
-    await message.answer("Бот работает! Ваше сообщение: " + message.text)
+    await message.answer("Ты написал: " + message.text)
 
-print("Бот запущен и слушает сообщения")
+print("Бот запущен и готов к работе!")
 bot.run()
